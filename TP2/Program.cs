027b2f595f596d443dbd3639346cdd08437be2c8
SantiagoVillar7 <santiagoytvc@gmail.com>
2025-09-10 02:43:35 +0200

    segunda subida

--- TP2/Program.cs
+++ TP2/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using dominio;
+using Negocio;
 namespace TP2
 {
     internal static class Program
